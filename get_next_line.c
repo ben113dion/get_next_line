bad28b395af4b588097147b03b9f34fc0b851251
Benjamin Dion <bdion@c2r6p4.42quebec.com>
2022-04-29 16:53:09 -0400

    auto-commit

--- get_next_line.c
+++ get_next_line.c
@@ -5,7 +5,7 @@
 #include <string.h>
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 10
+# define BUFFER_SIZE 20
 #endif
 
 void	ft_strset(char *b, char c)
@@ -17,12 +17,15 @@ void	ft_strset(char *b, char c)
 		b[i] = c;
 }
 
-int     is_newline(char *str)
+int     where_the_line(char *str)
 {
-    int i = -1;
-    while (str[++i] != '\n')
-        if (i == strlen(str))
-            return (0);
+    int i = 0;
+    while (str[i])
+    {
+		if (str[i] == '\n')
+			return(i);
+		i++;
+	}
     return (i);
 }
 
@@ -64,18 +67,16 @@ int main(void)
     char  *rtn;
     char  err = 0;
 
-    //rtn = get_next_line(fd);
-    //printf("test->|%s|\n", substr("12345678901234567890", 1, 10));
-    //rtn = get_next_line(fd);
-   // printf("ret->|%s|\n", rtn);
-    //rtn = get_next_line(fd);
-    //printf("ret->|%s|\n", rtn);
-    while (err != 4)
+
+    while (err <= 10)
     {
         rtn = get_next_line(fd);
-		printf("->%s", rtn);
-        err++;
+		printf("->'%s'\n\n", rtn);
+		if (rtn == NULL)
+			err = 44;
+		err++;
     }
+	free(rtn);
     return (0);
 }
 
@@ -89,28 +90,36 @@ int main(void)
 
 char    *get_next_line(int fd)
 {
-	char *buff = (char *) malloc(BUFFER_SIZE * sizeof(char)); //secu
+	char *buff = (char *) malloc(BUFFER_SIZE * sizeof(char) *2); //secu
 	int rtn_size = 0; //secu
-	char *rtn = (char *) malloc(BUFFER_SIZE * sizeof(char));
+	char *rtn = (char *) malloc(BUFFER_SIZE * sizeof(char) + 9999);
 	int readlen;
-	static char last_extra[BUFFER_SIZE + BUFFER_SIZE + 1];
+	static char last_extra[BUFFER_SIZE * BUFFER_SIZE + 1];
+	static int	exit = 0;
 	int nnewline;
   
  	// #1 (#4)regarder si il y a une string dans la variable static(last_extra)
 	
     if (strlen(last_extra) != 0)
 	{
-		nnewline = is_newline(last_extra);
-		if (strchr(last_extra, '\n') != NULL)
+		printf(".%s.", last_extra);
+		nnewline = where_the_line(last_extra);
+		if (strchr(last_extra, '\n') != NULL) // appeler si il y a au moin 1 \n 
 		{
-			printf("!");
-			ft_strset(rtn, '\0');
-			strcpy(rtn, substr(last_extra, 0, nnewline + 1 ));
-			strcpy(last_extra , last_extra + (nnewline + 1 ) );
+			printf("$1");
+			// ici cest parcequ'il y a un ou plus d'un \n dans notre last_extra
+			// ont doit donc 
+			strcpy(rtn, substr(last_extra, 0, where_the_line(last_extra) + 1 ));
+			//ft_strset(last_extra, '\0');
+			// ont doit supprimer les where_the_line(last_extra) premier char
+			strcpy(last_extra , substr(last_extra, where_the_line(last_extra) + 1, strlen(last_extra)) );
+			//printf("@%s@", last_extra);
+			free(buff);
+			return (rtn);
 		}
 		else
 		{
-			printf("!");
+			printf("$2");
 			strcpy(rtn, last_extra);
 			ft_strset(last_extra, '\0');
 		}
@@ -120,16 +129,27 @@ char    *get_next_line(int fd)
 	
 	/////////read section//////////
 	readlen = read(fd, buff, BUFFER_SIZE);
-	if (readlen == 0)
-		printf("$(return NULL)");
-	if (readlen < BUFFER_SIZE)
-		printf("$(last read so we return someting here)");
-	while ((strchr(buff, '\n') == NULL) && (readlen == BUFFER_SIZE)) 
+	if (readlen <= 0 || exit == 1)
+		return (NULL);
+
+	while ((strchr(buff, '\n') == NULL) && (readlen != 0)) 
 	{
-		printf("$(no \n here so we gonna place all in the rtn and read an other buffer");
-		readlen s= 9;//needed to be remove(its for exitting this) !!!!
+		// copier tout du buff dans le rtn
+		printf("$3");
+		strcat(rtn, buff);
+		readlen = read(fd, buff, BUFFER_SIZE);
 	}
-JSUISRENDUICI
+// ici ont peut dire quil nous reste juste a separer les buffers qui sont avec un '\n',
+// retourner tout jusquau '\n' 
+	if ( (strchr(buff, '\n') != NULL) && (readlen != 0) )
+	{
+		printf("\n\nDEBUG(((\n where_the_line(buff) = %d\n buff = '%s'  \n)))\n\n", where_the_line(buff), buff);
+		// couper tout quesuia avant le \n et le strcat dans rtn
+		// mettre le reste dans last_extra et retourner apres free le buff
+		printf("$4");
+		strcat(rtn, substr(buff, 0, where_the_line(buff)+1));
+		strcpy(last_extra, strchr(buff, '\n')+1);
+	}
 
 	////////////////////////////////
 	
